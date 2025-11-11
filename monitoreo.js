5e4fbbff103b965594620f451bb99a74a146da48
Erick Natanael Samperio Maldonado <133932596+SuperMKCG@users.noreply.github.com>
2025-11-11 10:34:28 -0600

    Update monitoreo.js

--- monitoreo.js
+++ monitoreo.js
@@ -1,5 +1,5 @@
-const API_URL = 'http://52.54.157.92:5500/api';
-const WS_URL = 'ws://52.54.157.92:5501/ws';
+const API_URL = 'https://silencesuzuka.duckdns.org/api';
+const WS_URL  = 'wss://silencesuzuka.duckdns.org/ws';
 const DISPOSITIVO_ID = 1;
 
 let websocket = null;
