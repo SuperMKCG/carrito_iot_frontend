4b0ab1013f4f090f798c3c301ecf3784ecac712f
Erick Natanael Samperio Maldonado <133932596+SuperMKCG@users.noreply.github.com>
2025-11-11 18:28:49 -0600

    Actualizacion url wss

--- monitoreo.js
+++ monitoreo.js
@@ -1,5 +1,5 @@
 const API_URL = 'https://silencesuzuka.duckdns.org/api';
-const WS_URL  = 'wss://silencesuzuka.duckdns.org/ws';
+const WS_URL  = 'wss://silencesuzuka.duckdns.org:5501/ws';
 const DISPOSITIVO_ID = 1;
 
 let websocket = null;
